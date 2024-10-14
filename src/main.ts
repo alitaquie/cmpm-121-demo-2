204e929f810aa73f80e8ded1751ec49386b1fbd9
ali <alitaquie51@gmail.com>
2024-10-14 15:34:39 -0700

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,17 @@ const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+
+const titleElement = document.createElement("h1");
+titleElement.textContent = APP_NAME;
+
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "appCanvas"; 
+
+
+app.appendChild(titleElement);
+app.appendChild(canvas);
